a042976476bd071157c00b7c67f255b0619865d5
Vladimir Gurinenko <vgurinenko@gmail.com>
2023-05-07 15:12:27 +0300

    Added demo.cy.js to project.

---
+++ cypress/e2e/demo.cy.js
@@ -0,0 +1,14 @@
+describe('Demo Tests', () => {
+
+    it('Opens the Home page', () => {
+  
+      cy.visit('https://www.demoblaze.com')
+  
+  
+      cy.get('#nava')
+  
+        .should('contain', 'PRODUCT STORE')
+  
+    })
+  
+  })  
