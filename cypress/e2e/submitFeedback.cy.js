608bf0fa11e8f43b5a150df543fa8d1f4960df5b
Vladimir Gurinenko <vgurinenko@gmail.com>
2023-06-01 11:50:19 +0300

    Update submitFeedback.cy.js

--- cypress/e2e/submitFeedback.cy.js
+++ cypress/e2e/submitFeedback.cy.js
@@ -9,6 +9,12 @@ describe('Demo Tests - Feedback', () => {
         .should('contain', 'PRODUCT STORE')
 
       cy.get('#navbarExample > ul > li:nth-child(2) > a').click()
+
+      // Fill out the form and click "Send message"
+      cy.get('#recipient-email').type('user@example.com')
+      cy.get('#recipient-name').type('John Doe')
+      cy.get('#message-text').type('Hi there! This is a test message...')
+      cy.get('#exampleModal > div > div > div.modal-footer > button.btn.btn-primary').click()
   
     })
   
