5b11b1d2ae3af7c90a27f3f7507ee630119fc5e2
Vladimir Gurinenko <vgurinenko@gmail.com>
2023-06-29 15:58:14 +0300

    Updated selectors
    Replaced automatically generated with readable ones.

--- cypress/e2e/orderAnyProduct.cy.js
+++ cypress/e2e/orderAnyProduct.cy.js
@@ -8,30 +8,54 @@ describe('Order any product', () => {
             .should('contain', 'PRODUCT STORE')
 
         // Click on the first product link...
-        cy.get('#tbodyid > div:nth-child(1) > div > div > h4 > a').click()
+        // cy.get('#tbodyid > div:nth-child(1) > div > div > h4 > a').click()
+        cy.get('.hrefch').first().click()
 
-        // Check that we are on the product page - Add to cart button should exist
-        cy.contains('Add to cart').click()
+        // Check that we are on the product page - [Add to cart] button should exist
+        // cy.get('#tbodyid > div.row > div > a')
+        //    .should('exist')
+        //    .click()
+
+        cy.get('a').contains('Add to cart').click()
 
         // Open the cart...
         cy.get('#cartur')
-            .should('contain', 'Cart').click()
+            .should('contain', 'Cart')
+            .click()
 
         // Check that the cart is not empty (contains at least 1 "Delete")
-        cy.contains('Delete')
+        // cy.contains('Delete')
+
+        // cy.get('#tbodyid > tr:nth-child(1) > td:nth-child(4) > a')
+        //     .should('exist')
+        //     .and('have.length', 1)
+
+        cy.get('.success').should('exist')
 
         // Click the Place Order button
-        cy.get('#page-wrapper > div > div.col-lg-1 > button').click()
+        // cy.get('#page-wrapper > div > div.col-lg-1 > button').click()
+        cy.get('button').contains('Place Order').click()
 
         // Fill out the order form
-        cy.get('#name').type('testName')
-        cy.get('#country').type('Norway')
-        cy.get('#city').type('Bergen')
-        cy.get('#card').type('1111222233334444')
-        cy.get('#month').type('07')
-        cy.get('#year').type('2025')
+        cy.get('#name')
+            .invoke('val', 'testName')
+        
+        cy.get('#country')
+            .invoke('val', 'Turkey')
+        
+        cy.get('#city')
+            .invoke('val', 'Antalya')
+        
+        cy.get('#card')
+            .invoke('val', '1111222233334444')
+        
+        cy.get('#month')
+            .invoke('val', '07')
+        
+        cy.get('#year')
+            .invoke('val', '2025')
 
-        // Click the Purchase button
+        // Click the Purchase buttonx
         cy.contains('Purchase').click()
     })
   
